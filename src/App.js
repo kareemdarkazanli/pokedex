6c460797e8e47a2f7c7b2e128879a02526c90f26
kareemdarkazanli <kareemdarkazanli@users.noreply.github.com>
2019-04-24 20:33:44 -0700

    App should be simpler
    Let's have app only render the complex component called "Pokedex" which will host the "PokemonList" component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import Pokemon from './components/Pokemon'
+import Pokedex from './components/Pokedex'
 import './App.css'
 
 class App extends React.Component{
-
-  state = {
-    pokemons: [],
-    isLoaded: false
-  }
-
-  componentDidMount () {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then( pokemon => pokemon.json())
-      .then( (pokemon) =>{
-        this.setState({
-          pokemons: [].concat(...pokemon.results),
-          isLoaded: true
-        })
-      }
-      ).catch(error => {
-       console.log(error)
-    })
-  }
-  render ()  {
-
-    const pokeList = this.state.pokemons.map((pokemon,i) => (
-      <Pokemon name={pokemon.name} id={i + 1} url={pokemon.url}/>
-    ));
-    return (
-      <div className="App">
-        <div className="pokemon-list">
-          {this.state.isLoaded ? pokeList : <div>Loading</div>}
-        </div>
-      </div>
-    );
+  
+  render() {
+    <Pokedex />
   }
 }
 
